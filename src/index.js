67c6f0777cca7d9400e8fabcdd11516b1bcff824
yaredow <yaredyilma11@gmail.com>
2023-07-12 09:29:03 +0300

    add a second version of the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import StarRating from './StarRating';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App-V2 />
   </React.StrictMode>
 );
